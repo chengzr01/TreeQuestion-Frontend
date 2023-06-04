4a5354502555f3242cd7c8714a5924c756bce81c
chengzr01 <ziruicheng01@gmail.com>
2023-06-04 20:10:52 +0800

    modify url

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,11 +3,11 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     createProxyMiddleware("/user", {
-      target: "http://127.0.0.1:8000",
+      target: "http://18.206.229.27:8000",
       changeOrigin: true,
     }),
     createProxyMiddleware("/tree", {
-      target: "http://127.0.0.1:8000",
+      target: "http://18.206.229.27:8000",
       changeOrigin: true,
     })
   );
